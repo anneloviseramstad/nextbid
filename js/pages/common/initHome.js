b73c35ca9359a3a20adacc46423e952ce69793dc
anneloviseramstad <160476303+anneloviseramstad@users.noreply.github.com>
2025-05-18 12:35:19 +0200

    Functions for get started-btn - Adde dfunctions for redirecting when click get started. Create for logged in, register for logged out user.

--- js/pages/common/initHome.js
+++ js/pages/common/initHome.js
@@ -1,7 +1,16 @@
 import { displayListings } from "../../handlers/listings/displayListings.js";
+import { isAuthenticated } from "../../utils/storage.js";
 
 export function initHome() {
   displayListings();
+
+  const getStartedBtn = document.getElementById("getStartedBtn");
+  if (getStartedBtn) {
+    getStartedBtn.href = isAuthenticated()
+      ? "/create/index.html"
+      : "/register/index.html";
+  }
+
   const inputs = ["searchInput", "searchUsernameInput", "sortByTag"];
   inputs.forEach((id) => {
     const el = document.getElementById(id);
